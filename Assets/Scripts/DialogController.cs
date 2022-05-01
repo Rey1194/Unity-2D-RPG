40708581ae011375b3927351fd3268b7d5a0be7b
Yandri <yandridavidr@gmail.com>
2022-05-01 17:09:14 -0500

    Dialog Controller & arrays

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// reference the UI
+using UnityEngine.UI;
+// reference Text Mesh Pro
+using TMPro;
+
+public class DialogController : MonoBehaviour
+{
+	// Editor's Reference
+	[SerializeField] TextMeshProUGUI dialogText, nameText;
+	[SerializeField] GameObject dialogBox, nameBox;
+	// variables
+	[SerializeField] string[] dialogSentence;
+	[SerializeField] int currentSentence;
+	
+  // Start is called before the first frame update
+	void Start() {
+		// set the sentences based on the array index
+	  dialogText.text = dialogSentence[currentSentence];
+  }
+
+  // Update is called once per frame
+  void Update() {
+	  // Check if the dialog box is active
+	  if (dialogBox.activeInHierarchy) {
+	  	// check if click the fire button
+	  	if (Input.GetButtonUp("Fire1")) {
+	  		// increase the current sentence value
+	  		currentSentence++;
+	  		// if currente sentence is greater than dialog sentences
+	  		if (currentSentence >= dialogSentence.Length) {
+	  			// deactivate the dialog box
+	  			dialogBox.SetActive(false);
+	  		}
+	  		else {
+		  		// set the sentences based on the array index
+		  		dialogText.text = dialogSentence[currentSentence];
+	  		}
+	  	}
+	  }
+  }
+}
---
+++ Assets/Scripts/Level Manager/AreaExit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Scene manager
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+	// Variables
+	[SerializeField] private string sceneToLoad;
+	[SerializeField] private string transitionName;
+	[SerializeField] AreaEnter theAreaEnter;
+	
+  // Start is called before the first frame update
+	void Start() {
+		// At  the start set the transition name to this name
+	  theAreaEnter.transitionAreaName = this.transitionName;
+  }
+
+	// Sent when another object enters a trigger collider attached to this object (2D physics only).
+	protected void OnTriggerEnter2D(Collider2D other) {
+		// Check if collide with the player
+		if (other.CompareTag("Player")) {
+			// trigger the fade_start animation
+			MenuManager.instance.FadeImage();
+			// Set the transition name to this new transition name
+			PlayerMove.instance.transitionName = this.transitionName;
+			// Invoke the Coroutine
+			StartCoroutine(LoadSceneCoroutine());
+		}
+	}
+	
+	// IEnumerator to wait for the change of scene
+	IEnumerator LoadSceneCoroutine() {
+		// wait for one second
+		yield return new WaitForSeconds(1f);
+		// then load the new scene
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
