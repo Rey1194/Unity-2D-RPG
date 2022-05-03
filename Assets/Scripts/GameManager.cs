c7c862f59f97cd0c6d58deea13216b38b7dd1207
Yandri <yandridavidr@gmail.com>
2022-05-03 15:35:52 -0500

    creating the player menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] PlayerStats[] playerStats;
+	public static GameManager instance;
+  // Start is called before the first frame update
+  void Start() {
+  	if (instance != null && instance != this) {
+  		Destroy(this.gameObject);
+  	}
+  	else {
+  		instance = this;
+  	}
+  	DontDestroyOnLoad(this.gameObject);
+  	// Find the references in the editor
+  	playerStats = FindObjectsOfType<PlayerStats>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    
+  }
+}
