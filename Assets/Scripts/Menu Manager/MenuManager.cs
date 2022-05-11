b98520f2bed4463c20fc5b86bd3deaaabcb7906d
David Reyes <dreyes@fidenslat.com>
2022-05-10 19:41:47 -0500

    creating the player stats menu

--- Assets/Scripts/Menu Manager/MenuManager.cs
+++ Assets/Scripts/Menu Manager/MenuManager.cs
@@ -10,11 +10,27 @@ public class MenuManager : MonoBehaviour
 	public static MenuManager instance;
 	// Editor's reference
 	[SerializeField] private Image imageToFade;
+	[SerializeField] private GameObject menuPanel;
 	
 	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
 	protected void Start() {
 		instance = this;
 	}
+	
+	// Update is called every frame, if the MonoBehaviour is enabled.
+	protected void Update() {
+		// check for input and the menu is not active
+		if (Input.GetKeyDown(KeyCode.M) && menuPanel.activeInHierarchy == false) {
+			// Activate the menu 
+			menuPanel.SetActive(true);
+		}
+		// check for input and the menu is active
+		else if (Input.GetKeyDown(KeyCode.M) && menuPanel.activeInHierarchy == true) {
+			// Deactivate the menu
+			menuPanel.SetActive(false);
+		}
+	}
+	
 	// Method to trigger the animation
 	public void FadeImage() {
 		// Get the animator component an trigger the animation
