4b33acc5ccfb0fd939d062f3af16cf39100f9c6a
David Reyes <68441783+Rey1194@users.noreply.github.com>
2023-03-11 10:54:25 -0500

    mage logic

--- Assets/Scripts/Enemies/Mage.cs
+++ Assets/Scripts/Enemies/Mage.cs
@@ -9,6 +9,7 @@ public class Mage : MonoBehaviour
 	public Transform bulletPoint;
 	public float playerDistance;
 	public float shootCadency = 15f;
+	private float distance;
 	//private float waitShoot = 0;
 	
 	public GameObject fireBall;
@@ -23,27 +24,28 @@ public class Mage : MonoBehaviour
     // Update is called once per frame
 	void FixedUpdate()
 	{
-		float distance = Vector3.Distance(playerPos.position, enemyPos.position);		
-		if (distance > playerDistance || distance < -playerDistance)
-		{
-			StartCoroutine(Teleport(2.0f));
-		}
+		// IF THE DISTANCE IS GREATER
+		// TRANSPORT
+		// WAIT A SECOND
+		// IF THE DISTANCE IS NEAR
+		// SHOOT
+		// WAIT A SECOND AND LOOP
 		
-		if (distance < playerDistance || distance >  -playerDistance)
-		{
-			StartCoroutine(Attack(2.0f));
-		}
+		//  crear un timer y llamar a la rutina dependiendo de la distancia
+		StartCoroutine(Teleport(2.0f));
     }
 	// Transportarse cerca del jugador
 	private IEnumerator Teleport(float waitTime) 
 	{
+		// posicion del jugador
+		distance = Vector3.Distance(playerPos.position, enemyPos.position);		
+		// Transportarse despues de unos segundos
 		yield return new WaitForSeconds(waitTime);
 		this.transform.position = new Vector3(
 			playerPos.position.x - 3f, 
 			this.transform.position.y, 
 			this.transform.position.z
 		);
-		//Debug.Log("Transportado");
 	}
 	// Atacar al jugador
 	private IEnumerator Attack(float waitForShoot)
